e7b0d089c35f29f30cd6b42a4643960bf74bab86
Siim Schults <siim.schults@gmail.com>
2017-04-22 19:38:08 +0300

    new learning function with accuracy

--- code/main/titanic.py
+++ code/main/titanic.py
@@ -45,15 +45,15 @@ def clean_data(data_filename):
 def get_features_labels(data):
     return data[['Pclass', 'Age', 'Fare', 'Sex', 'Sex2']].as_matrix(), data[['Survived']].as_matrix()
 
-def learn(df):
+def learn_matrix_multiplication_model(df):
     # Model parameters
     W = tf.Variable(tf.ones([5, 1]), tf.float32)
     b = tf.Variable(tf.ones(1), tf.float32)
     # Model input and output
     x = tf.placeholder(tf.float32, shape=(None, 5))
-    linear_model = tf.matmul(x, W) + b
     y = tf.placeholder(tf.float32, shape=(None, 1))
 
+    linear_model = tf.matmul(x, W) + b
     loss = tf.reduce_sum(tf.abs(linear_model-y))
     # optimizer
     optimizer = tf.train.GradientDescentOptimizer(0.01)
@@ -78,6 +78,44 @@ def learn(df):
     curr_W, curr_b, curr_loss = sess.run([W, b, loss], {x: x_train, y: y_train})
     print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
     # evaluate training accuracy
+
+def learn(df):
+    # Model parameters
+    W = tf.Variable(tf.ones([5, 1]), tf.float32)
+    b = tf.Variable(tf.ones(1), tf.float32)
+    # Model input and output
+    x = tf.placeholder(tf.float32, shape=(None, 5))
+    y = tf.placeholder(tf.float32, shape=(None, 1))
+
+    linear_model = tf.nn.sigmoid(tf.matmul(x, W) + b)
+    loss = tf.reduce_sum(tf.abs(linear_model - y))
+    accuracy = tf.abs(linear_model - y) # reduce_count_zeros(linear_model - y < threshold) / len(y)
+    accuracy = tf.count_nonzero(accuracy < 0.2) / 714
+    # optimizer
+    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    train = optimizer.minimize(loss)
+
+    # Our data example
+    x_train, y_train = get_features_labels(df)
+    # training loop
+    # accuracy = tf.contrib.metrics.accuracy(y, linear_model)
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+        range_val = 2000
+        batch_size = 10
+        for i in range(range_val):
+            for batch_start in range(0, len(x_train) - batch_size, batch_size):
+                batch_end = batch_start + batch_size
+                features = x_train[batch_start:batch_end]
+                labels = y_train[batch_start:batch_end]
+                sess.run([train], {x: features, y: labels})
+
+
+        curr_W, curr_b, curr_loss, prediction = sess.run([W, b, loss, accuracy], {x: x_train, y: y_train})
+        print("Accuracy %s " % prediction)
+        print("W: %s b: %s loss: %s" % (curr_W, curr_b, curr_loss))
+        # evaluate training accuracy
 
 
 def main():
