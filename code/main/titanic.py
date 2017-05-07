e5f52beb5b0ede7db8a593a2795d8caaa0b2e073
Mihkel Lukats <mihkel.lukats@gmail.com>
2017-05-07 21:33:37 +0300

    Tanels special loss

--- code/main/titanic.py
+++ code/main/titanic.py
@@ -1,7 +1,4 @@
-import os
-
 import pandas as pd
-import numpy as np
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelBinarizer
@@ -11,18 +8,24 @@ pd.set_option('display.width', 100000)
 resource_path = '../../data/'
 file_name = 'picled_data.pickle'
 
+SEX = 'Sex'
+AGE = 'Age'
+CLASS = 'Pclass'
+FARE = 'Fare'
+AGE_MISSING = 'AgeMissing'
 
 def normalize(df):
     AGE = 'Age'
     ageMax = df[AGE].max()
     ageMin = df[AGE].min()
     df[AGE] = (df[AGE] - ageMin) / (ageMax - ageMin)
-    df[AGE] = (df[AGE]**0.8 * 2 - 1)
+    df[AGE] = (df[AGE] ** 0.8 * 2 - 1)
 
     FARE = 'Fare'
     fareMax = df[FARE].max()
     fareMin = df[FARE].min()
     df[FARE] = (df[FARE] - fareMin) / (fareMax - fareMin)
+    df[FARE] = df[FARE] * 2 - 1
 
     return df
 
@@ -31,19 +34,18 @@ def clean_data(data_filename):
     file_path = resource_path + file_name
     # if not os.path.exists(file_path):
     df = pd.read_csv('../../data/' + data_filename)
-    SEX = 'Sex'
-    AGE = 'Age'
-    df = df[['Survived', 'Pclass', AGE, 'Fare', SEX]]
-    age_missing = 'AgeMissing'
-    df[age_missing] = df[AGE].isnull()
-    df[age_missing] = LabelBinarizer().fit_transform(df[age_missing]) * 2 - 1
+
+
+    df = df[['Survived', CLASS, AGE, FARE, SEX]]
+
+    df[AGE_MISSING] = df[AGE].isnull()
+    df[AGE_MISSING] = LabelBinarizer().fit_transform(df[AGE_MISSING]) * 2 - 1
 
     df = df.fillna(method='bfill')
     df = df.fillna(method='ffill')
 
     df[SEX] = LabelBinarizer().fit_transform(df[SEX])
     df[SEX] = df[SEX] * 2 - 1
-
 
     df = normalize(df)
 
@@ -52,7 +54,7 @@ def clean_data(data_filename):
 
 
 def get_features_labels(data):
-    return data[['Sex']].as_matrix(), data[['Survived']].as_matrix()
+    return data[[SEX, CLASS, AGE, AGE_MISSING, FARE]].as_matrix(), data[['Survived']].as_matrix()
 
 
 def learn(df, test_set):
@@ -62,67 +64,59 @@ def learn(df, test_set):
     x_train, y_train = get_features_labels(df)
     x_test, y_test = get_features_labels(test_set)
 
-
-
+    learning_rate = tf.placeholder(tf.float32)
     inputs = tf.placeholder(tf.float32, shape=(None, x_train.shape[1]))
     labels = tf.placeholder(tf.float32, shape=(None, 1))
     # make forward pass of layer
-    # layer_sigmoid = tf.contrib.layers.fully_connected(inputs, 1,
-    #                                                   activation_fn=tf.nn.softplus,
-    #                                                   weights_initializer=tf.zeros_initializer())
+    layer_sigmoid = tf.contrib.layers.fully_connected(inputs, 4,
+                                                      activation_fn=tf.nn.softplus,
+                                                      weights_initializer=tf.random_normal_initializer())
 
     # layer_sigmoid = tf.contrib.layers.fully_connected(inputs, 2,
     #                                                   activation_fn=tf.nn.softplus,
-    #                                                   weights_initializer=tf.zeros_initializer())
+    #                                                   weights_initializer=tf.random_normal_initializer())
 
-    # layer_sigmoid = tf.contrib.layers.fully_connected(inputs, 3,
-    #                                                   activation_fn=tf.nn.softplus,
-    #                                                   weights_initializer=tf.zeros_initializer())
-
-    logits = tf.contrib.layers.fully_connected(inputs, 1,
+    logits = tf.contrib.layers.fully_connected(layer_sigmoid, 1,
                                                activation_fn=tf.sigmoid,
-                                               weights_initializer=tf.zeros_initializer())
+                                               weights_initializer=tf.random_normal_initializer())
     # accuracy
     accuracy = tf.abs(logits - labels)
     accuracy = tf.count_nonzero(accuracy < 0.5) / tf.placeholder(tf.int64, name='input_size')
 
     # training
-    loss = tf.reduce_sum(tf.abs(logits - labels))
-    optimizer = tf.train.GradientDescentOptimizer(0.01)
+    # survived = [logits[i] for i in range(labels.shape[0]) if labels[i][0] == tf.constant(1., dtype=tf.float32)]
+
+    # loss = tf.reduce_sum(tf.abs(logits - labels))
+    loss = (tf.reduce_sum(labels * (1 - logits)) / tf.reduce_sum(labels) + \
+            tf.reduce_sum((1 - labels) * logits) / tf.reduce_sum(1 - labels)) * 0.5
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate)
     train = optimizer.minimize(loss)
 
     # running graph
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
         batch_size = 10
-        for i in range(50):  # 2 rounds of training
+        for i in range(1000):  # 2 rounds of training
             for batch_start in range(0, len(x_train) - batch_size, batch_size):
                 batch_end = batch_start + batch_size
                 features = x_train[batch_start:batch_end]
                 labels_ = y_train[batch_start:batch_end]
                 sess.run(train, {inputs: features,
-                                 labels: labels_})
+                                 labels: labels_,
+                                 learning_rate: 0.1})
 
-            sigmoid_layer_val, ran_loss_val, accuracy_val = sess.run([inputs, loss, accuracy],
-                                                         {inputs: x_test, labels: y_test,
-                                                          'input_size:0': y_test.shape[0]})
-            print(i, 'accuracy/loss:', accuracy_val, ran_loss_val)
+            ran_loss_val, accuracy_val = sess.run([loss, accuracy],
+                                                  {inputs: x_test, labels: y_test,
+                                                   'input_size:0': y_test.shape[0]})
+            print(i, '1 - loss/accuracy:', 1 - ran_loss_val, accuracy_val)
 
 
 def main():
     data = clean_data("train.csv")
     print('data-size-total:', data.shape[0])
-    print(data[data['Survived'] == 1].shape[0])
     train_set, test_test = train_test_split(data, random_state=1)
-    learn(train_set, test_test)
-    # print(df[['Survived']].as_matrix().transpose())
-    # i = 0
-    # for row in df.iterrows():
-    #     print(row)
-    #     i += 1
-    #     if i == 4:
-    #         break
 
+    learn(train_set, test_test)
 
 if __name__ == "__main__":
     main()
